7236bae567a20605a7280c4d2ec6403604ef01ef
Gayatri Dongare <gayatridongare6@gmail.com>
2026-03-20 23:09:13 +0530

    fixed signup route properly

--- Frontend/HiddenHunt/src/api/auth.js
+++ Frontend/HiddenHunt/src/api/auth.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hiddenhunt-backend.onrender.com",
+  baseURL: "https://hiddenhunt-backend.onrender.com/api",
 });
 
 // REGISTER
-export const registerUser = async (data) => {
-  return API.post("/api/auth/register", data);
+export const registerUser = (data) => {
+  return API.post("/auth/register", data);
 };
 
 // LOGIN
-export const loginUser = async (data) => {
-  return API.post("/api/auth/login", data);
+export const loginUser = (data) => {
+  return API.post("/auth/login", data);
 };
 
 // VERIFY OTP
-export const verifyOtp = async (data) => {
-  return API.post("/api/auth/verify-otp", data);
+export const verifyOtp = (data) => {
+  return API.post("/auth/verify-otp", data);
 };
